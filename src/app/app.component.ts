7257c33577985fe509c2f9761dac64fd88071d8e
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-08-08 19:21:57 -0300

    remove scss e adiciona css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'jv-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {}
